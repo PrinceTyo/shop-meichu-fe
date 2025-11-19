06c8f228902b430fbef42ca0f1694f6f382e22e7
PrinceTyo <kresnaale2@gmail.com>
2025-11-19 16:41:54 +0700

    feat: slicing and add animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import AdvantageSection from "@/components/sections/advantage-section";
+import TrendingSection from "@/components/sections/advantage-section";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-bermuda font-sans dark:bg-black">
+    <>
+    {/* <div className="flex min-h-screen items-center justify-center bg-bermuda font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
@@ -60,6 +63,8 @@ export default function Home() {
           </a>
         </div>
       </main>
-    </div>
+    </div> */}
+      <AdvantageSection/>
+      </>
   );
 }
