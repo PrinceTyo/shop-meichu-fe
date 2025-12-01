09ca436ba22b024f28bc43cc24536c13c145dc8c
PrinceTyo <kresnaale2@gmail.com>
2025-12-01 08:35:17 +0700

    feat: add responsive HeaderPage component for collections and about page

---
+++ src/app/collections/[slug]/page.tsx
@@ -0,0 +1,13 @@
+import HeaderPage from "@/components/header/header-page";
+
+export default function CollectionsPage() {
+  return (
+    <HeaderPage
+      type="collections"
+      img="/assets/gallery/girl3.jpg"
+      title="SEASONAL MUST-HAVES"
+      desc="Elevate your wardrobe with the latest essentials tailored for the season—handpicked styles
+            that blend comfort, trend, and timeless appeal."
+    />
+  );
+}
