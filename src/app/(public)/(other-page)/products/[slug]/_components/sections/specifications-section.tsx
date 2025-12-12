59908245fe857f11fe0afd7a4eafbc1a0ab44aae
YuzakiOnly <aryamenk2007@gmail.com>
2025-12-12 15:21:22 +0700

    fix: fix

--- src/app/(public)/(other-page)/products/[slug]/_components/sections/specifications-section.tsx
+++ src/app/(public)/(other-page)/products/[slug]/_components/sections/specifications-section.tsx
@@ -102,7 +102,7 @@ export default function SpecificationsSection({
   return (
     <div
       ref={ref}
-      className="text-white min-h-screen flex flex-col items-center justify-center space-y-8 relative px-4 md:px-4"
+      className="zzztext-white min-h-screen flex flex-col items-center justify-center space-y-8 relative px-4 md:px-4"
       style={background}
     >
       <h1 className="font-rubik text-4xl md:text-5xl lg:text-7xl font-bold text-center z-10">
